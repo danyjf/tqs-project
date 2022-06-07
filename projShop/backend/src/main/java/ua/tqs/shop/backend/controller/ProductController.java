c792ea70a20ab9cb789846d5d436ef559801e2ef
SousaUA <hsousa@ua.pt>
2022-06-07 20:06:51 +0100

    Basic Test

--- projShop/backend/src/main/java/ua/tqs/shop/backend/controller/ProductController.java
+++ projShop/backend/src/main/java/ua/tqs/shop/backend/controller/ProductController.java
@@ -8,7 +8,7 @@ import ua.tqs.shop.backend.services.ProductService;
 import javax.validation.Valid;
 import java.util.List;
 
-@CrossOrigin(origins = {"http://127.0.0.1:5500", "http://localhost:5500", "http://deti-engsoft-08:5500"})
+@CrossOrigin(origins = {"http://127.0.0.1:5500", "http://localhost:5500", "http://deti-tqs-15.ua.pt:5500"})
 @RestController
 public class ProductController {
     @Autowired
