4d46563b00343aba814f96447d964b4b594baf2d
Tiago Costa <bran.costa@ua.pt>
2022-06-15 16:26:26 +0100

    controller first version

--- projDeliveries/backend/src/main/java/pt/ua/tqs/backend/Controller/DeliveryController.java
+++ projDeliveries/backend/src/main/java/pt/ua/tqs/backend/Controller/DeliveryController.java
@@ -6,9 +6,10 @@ import org.springframework.web.bind.annotation.*;
 
 import pt.ua.tqs.backend.Service.DeliveryService;
 import pt.ua.tqs.backend.Model.Delivery;
-import pt.ua.tqs.backend.Model.User;
 
+import java.sql.Timestamp;
 import java.util.List;
+
 
 @RestController
 @RequestMapping("/")
@@ -17,20 +18,34 @@ public class DeliveryController {
     private DeliveryService ds;
     
     @PostMapping("/delivery")
-    public ResponseEntity<Delivery> create_delivery(){
+    public ResponseEntity<Delivery> create_delivery(@RequestBody String client_phone, @RequestBody String store_phone, @RequestBody String orderTime, @RequestBody String orderNote){
     //receive information for delivery, send to service, receive delivery and respond
-        return null;
+        Timestamp orderTimestamp = Timestamp.valueOf(orderTime);
+        Delivery d = ds.create_delivery(client_phone, store_phone, orderTimestamp, orderNote);
+        if(d != null){
+            return ResponseEntity.ok().body(d);
+        }
+        else{
+            return ResponseEntity.badRequest().body(null);
+        }
     }
 
     @GetMapping("/deliveries")
     public ResponseEntity<List<Delivery>> list_deliveries(){
     //ask service for deliveries, send response with deliveries
-        return null;
+        List<Delivery> ld = ds.list_deliveries();
+        return ResponseEntity.ok().body(ld);
     }
     
     @PostMapping("/delivery/rider")
-    public ResponseEntity<Delivery> assign_to_rider(){
+    public ResponseEntity<Delivery> assign_to_rider(@RequestBody long delivery_id, @RequestBody String rider_phone){
     //send rider and delivery information to service, receive updated delivery and respond
-        return null;
+        Delivery d = ds.assign_to_rider(delivery_id, rider_phone);
+        if(d != null){
+            return ResponseEntity.ok().body(d);
+        }
+        else{
+            return ResponseEntity.badRequest().body(null);
+        }
     }
 }
