8431b415d059c27fef39f1e1e479ee1363e658b2
Daniel Francisco <deny.jo@hotmail.com>
2022-06-16 17:41:03 +0100

    Remove unused file

--- projDeliveries/backend/src/test/resources/db/migration/V001__INIT.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE user (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL,
-    password VARCHAR(255) NOT NULL,
-    phone VARCHAR(255) NOT NULL,
-    userType VARCHAR(255) NOT NULL
-);
