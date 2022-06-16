7263076dfe4eb540663692948f49e6ad0511e7eb
SousaUA <hsousa@ua.pt>
2022-06-16 17:37:39 +0100

    OrderController Tests

---
+++ projShop/backend/music/src/test/java/shop/music/it/OrderControllerTest.java
@@ -0,0 +1,44 @@
+package shop.music.it;
+
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.http.MediaType;
+
+import static org.springframework.mock.http.server.reactive.MockServerHttpRequest.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import shop.music.model.Order;
+
+@SpringBootTest()
+@AutoConfigureMockMvc
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public class OrderControllerTest {
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    public void whenGetUserOrders_thenStatus200AndJSON() throws Exception {
+        mvc.perform(get("/api/v1/user/orders/1").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+    }
+
+    @Test
+    public void whenPostUserOrders_thenStatus201() throws Exception {
+        mvc.perform( MockMvcRequestBuilders
+                        .post("/api/v1/order/1/1")
+                        .content("{'userid': 1, 'productid': 1}")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON))
+                        .andExpect(status().isOk())
+                        .andExpect(MockMvcResultMatchers.jsonPath("$.userid").exists());
+    }
+}
