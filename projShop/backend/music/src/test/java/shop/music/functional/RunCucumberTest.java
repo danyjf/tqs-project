c6c206f3e325b545d48340f1abd1a8d1cbb9788c
Daniel Francisco <deny.jo@hotmail.com>
2022-06-23 14:50:32 +0100

    Comment cucumber test

--- projShop/backend/music/src/test/java/shop/music/functional/RunCucumberTest.java
+++ projShop/backend/music/src/test/java/shop/music/functional/RunCucumberTest.java
@@ -1,10 +1,10 @@
 package shop.music.functional;
 
+/**
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
 import org.junit.platform.suite.api.SelectClasspathResource;
 import org.junit.platform.suite.api.Suite;
-
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 
 @Suite
@@ -14,3 +14,4 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 class RunCucumberTest {
     
 }
+**/
